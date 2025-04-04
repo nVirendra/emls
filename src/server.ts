5d032245b77cdaec8a4cc0e295da21797bfc835c
Virendra Nishad <virendran818@gmail.com>
2025-04-04 22:52:48 +0530

    client url update for local & prod

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
     credentials: true,
   },
 });
@@ -19,6 +19,6 @@ export { io };
 const PORT = process.env.PORT || 5000;
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
   });
 });
